aa0436368d3cb7fab998577ed1f08d00bc7771ed
Anna Melnyk <blairw73@gmail.com>
2024-07-27 21:40:40 +1000

    Add quizz

--- promises.quizz.js
+++ promises.quizz.js
@@ -19,6 +19,8 @@ readFile("./files/demofile.txt", "utf-8")
   .then(
     (data) => console.log("File read JS", data),
     (err) => console.error("Oops Js", err.message)
+    // `then` can take two arguments, the second argument is a _error_ handler
+    // that gets called if the promise is `rejected`
   )
 
 // NodeJS way
@@ -30,3 +32,12 @@ readFileNode("./files/demofile.txt", "utf-8")
     (data) => console.log("File read Node", data),
     (err) => console.error("Oops Node", err.message)
   )
+
+
+  // Question 2
+  //Load a file from disk using readFile and then compress it using the async zlib node library,
+  // use a promise chain to process this work.
+
+  const fs = require("fs");
+  const zlib = require("zlib");
+
